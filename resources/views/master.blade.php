da1ecd145f6f08a51c93a4a911e103581a0c17ec
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2022-01-20 15:59:59 +0700

    Set Layout Dropdown Logout

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -28,23 +28,25 @@
                 <a href="#" class="burger-btn d-block d-xl-none">
                     <i class="bi bi-justify fs-3"></i>
                 </a>
-                <nav class="navbar navbar-expand navbar-light ml-auto">
+                <nav class="navbar navbar-expand navbar-light ml-auto ">
                     <div class="container-fluid">
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div class="dropdown">
+                            <div class="dropdown position-absolute" style="left: 85%">
                                 <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <div class="user-menu d-flex">
-                                        <div class="user-name text-end me-3">
-                                            <h6 class="mb-0 text-gray-600">
-                                                Tes
+                                    <div class="user-menu d-flex" style="border-radius: 20px; ">
+                                        <div class="user-name" style="padding-block: 10px; padding-inline: 40px"  >
+                                            <h6 class="mb-0 text-dark d-flex">
+                                                Bandot81 <i class="bi bi-caret-down-square-fill ms-2"></i>
                                             </h6>
                                             <p class="mb-0 text-sm text-gray-600">
                                                 Admin
                                             </p>
+                                            
                                         </div>
+                                        
                                     </div>
                                 </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton" >
                                     <li>
                                         <h6 class="dropdown-header">Hello, Tes
                                         </h6>
