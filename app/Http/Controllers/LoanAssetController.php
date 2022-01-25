c5408cf48bb0ae5bce748a9ad76799db8a19de75
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2022-01-25 15:47:40 +0700

    loan asset

---
+++ app/Http/Controllers/LoanAssetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LoanAsset;
+use Illuminate\Http\Request;
+
+class LoanAssetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LoanAsset  $loanAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LoanAsset $loanAsset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LoanAsset  $loanAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LoanAsset $loanAsset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LoanAsset  $loanAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LoanAsset $loanAsset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LoanAsset  $loanAsset
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LoanAsset $loanAsset)
+    {
+        //
+    }
+}
