b5367cda718582e07bdaac4cb14bdb02fdbd2203
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2022-02-08 22:10:59 +0700

    styling repair report excel

--- app/Exports/AssetServiceParameterExport.php
+++ app/Exports/AssetServiceParameterExport.php
@@ -10,26 +10,38 @@ use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Carbon\Carbon;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class AssetServiceParameterExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents, WithHeadingRow
+class AssetServiceParameterExport implements WithHeadings, ShouldAutoSize, WithEvents, WithHeadingRow, WithColumnWidths
 {
-    protected $fromDates,$toDates;
+    protected $fromDates, $toDates;
 
-    function __construct($fromDates,$toDates)
+    function __construct($fromDates, $toDates)
     {
         $this->fromDates = $fromDates;
         $this->toDates = $toDates;
     }
-    
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 3,
+            'B' => 6,
+            'C' => 5,
+            'E' => 30,
+            'G' => 25,
+            'H' => 25,
+        ];
+    }
+
     public function headings(): array
     {
         return [
-            'Nama Asset',
-            'Unit',
-            'Nama',
-            'Deskripsi Masalah',
-            'Diagnosa Perbaikan',
-            'Tanggal Selesai',
+            // 'Nama Asset',
+            // 'Unit',
+            // 'Nama',
+            // 'Deskripsi Masalah',
+            // 'Diagnosa Perbaikan',
+            // 'Tanggal Selesai',
         ];
     }
     /**
@@ -37,62 +49,174 @@ class AssetServiceParameterExport implements FromCollection, WithHeadings, Shoul
      */
     public function registerEvents(): array
     {
+        $center = [
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+        ];
         $styleArray = [
             'font' => [
                 'bold' => true,
-                'size' => 13
+                'size' => 12
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
             ],
         ];
         $styleTitle = [
             'font' => [
                 'bold' => true,
-                'size' => 15
+                'size' => 12
             ],
-            'alignment' => [
-                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+        ];
+        $styleTitleDate = [
+            'font' => [
+                'bold' => true,
+                'size' => 11
             ],
         ];
         $styleContent = [
             'font' => [
                 'bold' => false,
-                'size' => 12
+                'size' => 11
             ],
             'alignment' => [
-                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_JUSTIFY,
+                'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
             ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+
+                ],
+            ],
+
         ];
         return [
-            AfterSheet::class    => function (AfterSheet $event) use ($styleArray, $styleTitle,$styleContent) {
-                // $cellRange = 'A1:G1'; // All headers
-                $event->sheet->setCellValue('A1', 'Laporan Perbaikan Barang')->mergeCells("A1:H1")->getStyle('A1:H1')->applyFromArray($styleTitle);
-                $event->sheet->getStyle('A2:H2')->applyFromArray($styleArray);
-                $event->sheet->setCellValue('A2', 'No');
-                $event->sheet->setCellValue('B2', 'Nama Asset');
-                $event->sheet->setCellValue('C2', 'Unit');
-                $event->sheet->setCellValue('D2', 'Nama');
-                $event->sheet->setCellValue('E2', 'Deskripsi Masalah');
-                $event->sheet->setCellValue('F2', 'Diagnosa Perbaikan');
-                $event->sheet->setCellValue('G2', 'Tanggal Mulai');
-                $event->sheet->setCellValue('H2', 'Tanggal Selesai');
-                foreach (range('A', 'H') as $col) {
+            AfterSheet::class    => function (AfterSheet $event) use ($center, $styleArray, $styleTitle, $styleTitleDate, $styleContent) {
+                $event->sheet->getDelegate()->getRowDimension('10')->setRowHeight(27);
+                $event->sheet->getStyle('A1:A4')->applyFromArray($styleTitle);
+                $event->sheet->getStyle('E')->getAlignment()->setWrapText(true);
+                $event->sheet->getStyle('G')->getAlignment()->setWrapText(true);
+                $event->sheet->getStyle('H')->getAlignment()->setWrapText(true);
+                $event->sheet->setCellValue('A1', 'PT. Angkasa Pura Airports');
+                $event->sheet->setCellValue('A2', 'Cabang Bandara Juanda - Surabaya');
+                $event->sheet->setCellValue('A4', 'Laporan Perbaikan Asset');
+                $event->sheet->setCellValue('A6', 'Tanggal: ');
+                $event->sheet->setCellValue('C6', Carbon::now()->isoFormat('DD MMMM YYYY'))->getStyle('C6')->applyFromArray($styleTitleDate);
+                $event->sheet->setCellValue('I5', 'Periode: ');
+                $event->sheet->setCellValue('I6', Carbon::parse($this->fromDates)->isoFormat('DD MMMM YYYY') . ' - ' . Carbon::parse($this->toDates)->isoFormat('DD MMMM YYYY'))->getStyle('I6')->applyFromArray($styleTitleDate)->getAlignment()->setWrapText(false);
+
+                $event->sheet->setCellValue('B9', 'No')->mergeCells("B9:B10")->getStyle('B9:B10')->applyFromArray($styleArray);
+                $event->sheet->setCellValue('C9', 'Nama Asset')->mergeCells("C9:D10")->getStyle('C9:D10')->applyFromArray($styleArray);
+                $event->sheet->setCellValue('E9', 'Pengadu')->mergeCells("E9:F9")->getStyle('E9:F9')->applyFromArray($styleArray, $center);
+                $event->sheet->setCellValue('E10', 'Unit')->getStyle('E10')->applyFromArray($styleArray);
+                $event->sheet->setCellValue('F10', 'Nama')->getStyle('F10')->applyFromArray($styleArray);
+
+                $event->sheet->setCellValue('G9', 'Perbaikan')->mergeCells("G9:H9")->getStyle('G9:H9')->applyFromArray($styleArray, $center);
+                $event->sheet->setCellValue('G10', 'Deskripsi Masalah')->getStyle('G10')->applyFromArray($styleArray);
+                $event->sheet->setCellValue('H10', 'Diagnosa Perbaikan')->getStyle('H10')->applyFromArray($styleArray);
+
+                $event->sheet->setCellValue('I9', 'Tanggal')->mergeCells("I9:J9")->getStyle('I9:J9')->applyFromArray($styleArray, $center);
+                $event->sheet->setCellValue('I10', 'Tanggal Komplain')->getStyle('I10')->applyFromArray($styleArray);
+                $event->sheet->setCellValue('J10', 'Tanggal Selesai')->getStyle('J10')->applyFromArray($styleArray);
+
+                foreach (range('D', 'J') as $col) {
+                    if ($col === 'E') {
+                        continue;
+                    }
+                    if ($col === 'G') {
+                        continue;
+                    }
+                    if ($col === 'H') {
+                        continue;
+                    }
                     $event->sheet->getColumnDimension($col)->setAutoSize(true);
                 }
-                $cell=3;
-                $i=1;
-                $laporan = UnitLog::getServiceParameter($this->fromDates,$this->toDates);
-                foreach ($laporan as $row) {
-                    $event->sheet->getStyle('A'.$cell.':'.'H'.$cell)->applyFromArray($styleContent);
-                    $event->sheet->setCellValue('A' . $cell, $i);
-                    $event->sheet->setCellValue('B' . $cell, $row->asset_name);
-                    $event->sheet->setCellValue('C' . $cell, $row->department_name);
-                    $event->sheet->setCellValue('D' . $cell, $row->complainant_name);
-                    $event->sheet->setCellValue('E' . $cell, $row->desc_complain);
-                    $event->sheet->setCellValue('F' . $cell, $row->diagnose);
-                    $event->sheet->setCellValue('G' . $cell, Carbon::parse(date($row->created_at))->toFormattedDateString());
-                    $event->sheet->setCellValue('H' . $cell, Carbon::parse(date($row->date_fixed))->toFormattedDateString());
+                $cell = 11;
+                $i = 1;
+                $service_date = UnitLog::getDateParameter($this->fromDates, $this->toDates);
+                foreach ($service_date as $date) {
+                    $event->sheet->setCellValue('B' . $cell, $i . '. ');
+                    $event->sheet->setCellValue('C' . $cell, Carbon::parse($date->date_fixed)->isoFormat('DD MMMM YYYY'))->mergeCells('C' . $cell . ':J' . $cell);
                     $cell++;
+                    $services = UnitLog::getLogPerDate($date->date_fixed);
+                    $i_data = 1;
+                    foreach ($services as $row) {
+                        $cellData = $cell;
+                        $event->sheet->getStyle('B' . $cell . ':' . 'J' . $cellData)->applyFromArray($styleContent);
+                        $event->sheet->setCellValue('C' . $cellData, $i_data . '. ')->getStyle('C' . $cellData)->applyFromArray($center);
+                        $event->sheet->setCellValue('D' . $cellData, $row->asset_name);
+                        $event->sheet->setCellValue('E' . $cellData, $row->department_name)->getStyle('E' . $cellData)->applyFromArray($center);
+                        $event->sheet->setCellValue('F' . $cellData, $row->complainant_name);
+                        $event->sheet->setCellValue('G' . $cellData, $row->desc_complain);
+                        $event->sheet->setCellValue('H' . $cellData, $row->diagnose);
+                        $event->sheet->setCellValue('I' . $cellData, Carbon::parse($row->date)->isoFormat('DD MMMM YYYY'))->getStyle('I' . $cellData)->applyFromArray($center);
+                        $event->sheet->setCellValue('J' . $cellData, Carbon::parse($row->date_fixed)->isoFormat('DD MMMM YYYY'))->getStyle('J' . $cellData)->applyFromArray($center);
+                        $endRow = $cellData;
+
+                        // $event->sheet->setCellValue('C' . $cellData, $i_data);
+                        $cellData++;
+                        $cell = $cellData;
+                        $i_data++;
+                    }
                     $i++;
                 }
+
+                // --------content outside border-----------
+                // left
+                $event->sheet->getStyle('B11:B' . $endRow)->applyFromArray([
+                    'borders' => [
+                        'left' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                        ],
+                        'right' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                        ]
+                    ],
+                    'alignment' => [
+                        'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                    ],
+                ]);
+                //right
+                $event->sheet->getStyle('J11:J' . $endRow)->applyFromArray([
+                    'borders' => [
+                        'right' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                        ]
+                    ],
+                ]);
+                //bottom
+                $event->sheet->getStyle('B' . $endRow . ':J' . $endRow)->applyFromArray([
+                    'borders' => [
+                        'bottom' => [
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                        ]
+                    ],
+                ]);
+
+                // $laporan = UnitLog::getDateParameter($this->fromDates,$this->toDates);
+                // foreach ($laporan as $row) {
+                //     $event->sheet->getStyle('A'.$cell.':'.'H'.$cell)->applyFromArray($styleContent);
+                //     $event->sheet->setCellValue('A' . $cell, $i);
+                //     $event->sheet->setCellValue('B' . $cell, $row->asset_name);
+                //     $event->sheet->setCellValue('C' . $cell, $row->department_name);
+                //     $event->sheet->setCellValue('D' . $cell, $row->complainant_name);
+                //     $event->sheet->setCellValue('E' . $cell, $row->desc_complain);
+                //     $event->sheet->setCellValue('F' . $cell, $row->diagnose);
+                //     $event->sheet->setCellValue('G' . $cell, Carbon::parse(date($row->created_at))->toFormattedDateString());
+                //     $event->sheet->setCellValue('H' . $cell, Carbon::parse(date($row->date_fixed))->toFormattedDateString());
+                //     $cell++;
+                //     $i++;
+                // }
                 // $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
                 // $event->sheet->getStyle($cellRange)->ApplyFromArray($styleArray);
             },
@@ -104,6 +228,6 @@ class AssetServiceParameterExport implements FromCollection, WithHeadings, Shoul
     public function collection()
     {
         //return Loan::all();
-        return collect(UnitLog::getServiceParameter($this->fromDates,$this->toDates));
+        return collect(UnitLog::getServiceParameter($this->fromDates, $this->toDates));
     }
 }
--- app/Models/UnitLog.php
+++ app/Models/UnitLog.php
@@ -21,6 +21,56 @@ class UnitLog extends Model
     {
         return $this->belongsTo(ServiceAsset::class);
     }
+    public function getDate()
+    {
+        $current_month = Carbon::now()->format('m');
+        // dd($current_month);
+        $record = DB::table('unit_logs')
+            ->selectRaw('DISTINCT date_fixed')
+            ->whereMonth('date_fixed', $current_month)
+            ->orderBy('date_fixed', 'ASC')
+            ->get()->toArray();
+        return $record;
+    }
+    public function getDateParameter($fromDates, $toDates)
+    {
+        $current_month = Carbon::now()->format('m');
+        // dd($current_month);
+        if ($fromDates == $toDates) {
+            $record = DB::table('unit_logs')
+                ->selectRaw('DISTINCT date_fixed')
+                ->where('date_fixed', '=', $fromDates)
+                ->orderBy('date_fixed', 'ASC')
+                ->get()->toArray();
+            
+        } else {
+            $record = DB::table('unit_logs')
+                ->selectRaw('DISTINCT date_fixed')
+                ->where('unit_logs.date_fixed', '>=', $fromDates)
+                ->where('unit_logs.date_fixed', '<=', $toDates)
+                ->orderBy('date_fixed', 'ASC')
+                ->get()->toArray();
+        }
+        return $record;
+    }
+    public static function getLogPerDate($date)
+    {
+        $record = DB::table('unit_logs')->leftJoin('service_assets', 'unit_logs.asset_id', '=', 'service_assets.id')
+            ->leftJoin('departments', 'unit_logs.department_id', '=', 'departments.id')
+            ->select(
+                'service_assets.name as asset_name',
+                'departments.name as department_name',
+                'unit_logs.complainant_name as complainant_name',
+                'unit_logs.desc_complain as desc_complain',
+                'unit_logs.diagnose as diagnose',
+                'service_assets.date as date',
+                'unit_logs.date_fixed as date_fixed',
+                'unit_logs.created_at as created_at',
+            )
+            ->where('unit_logs.date_fixed', $date)
+            ->get()->toArray();
+        return $record;
+    }
     public static function getDataUnit()
     {
         $current_month = Carbon::now()->format('m');
